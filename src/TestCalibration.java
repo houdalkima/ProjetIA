311a7f7515630d906717b98edea44468d3335372
RobinChaussemy <chaussemy.robin@gmail.com>
2022-11-04 11:06:22 +0100

    Finalisation des des Test de calibration des capteur

--- src/TestCalibration.java
+++ src/TestCalibration.java
@@ -1,4 +1,3 @@
-package src;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Random;
@@ -6,6 +5,19 @@ import java.util.Random;
 import lejos.hardware.Button;
 
 public class TestCalibration {
+	private static float[] White = new float[] {0.2755f ,0.2794f,0.2451f};
+	private static float[] Blue = new float[] {0.0235f,0.0441f,0.0696f};
+	private static float[] Yellow = new float[] {0.2392f,0.2167f,0.0510f};
+	private static float[] Red = new float[] {0.1441f,0.0441f,0.0216f};
+	private static float[] Green = new float[] {0.0568f,0.1275f,0.04118f};
+	private static float[] Black = new float[] {0.0353f,0.0480f,0.0206f};
+	
+	public static boolean errorrange(float borne, float value, float e) {
+		if (e<0 || e>1) {
+			throw new IllegalArgumentException("Donner une valeur entre 0 et 1");
+		}
+		return (e*borne <= value && e/borne >= value);
+	}
 	
 	public static int genererInt(int borneInf, int borneSup){
 		   Random random = new Random();
@@ -33,45 +45,79 @@ public class TestCalibration {
 		}
 	}
 	
-	public static boolean TestDistance(Capteur c, int error) {
+	public static boolean TestFourDistance(Capteur c, float error) {
 		System.out.println("15 cm");
 		Button.ENTER.waitForPressAndRelease();
-		if (!(0.15*error <= c.getDistance() & c.getDistance() <0.15/error)) {
+		if (!(errorrange(0.15f,c.getDistance(),error))) {
 			return false;
 		}
 		System.out.println("30 cm");
 		Button.ENTER.waitForPressAndRelease();
-		if (!(0.3*error <= c.getDistance() && c.getDistance() <0.3/error)) {
+		if (!((errorrange(0.30f,c.getDistance(),error)))) {
 			return false;
 		}
 		System.out.println("50 cm");
 		Button.ENTER.waitForPressAndRelease();
-		if (!(0.5*error <= c.getDistance() && c.getDistance() <0.50/error)) {
+		if (!((errorrange(0.50f,c.getDistance(),error)))) {
 			return false;
 		}
 		System.out.println("80 cm");
 		Button.ENTER.waitForPressAndRelease();
-		if (!(0.8*error <= c.getDistance() && c.getDistance() <0.8/error)) {
+		if (!((errorrange(0.8f,c.getDistance(),error)))) {
 			return false;
 		}
 		return true;
 		
 	}
-	public static boolean TestColor(Capteur c) {
-		float[] Color = new float[3];
-		System.out.println("Blanc");
+	
+	public static boolean TestDistance(Capteur c,float distance ,float error) {
+		System.out.println(distance*10+" cm");
 		Button.ENTER.waitForPressAndRelease();
-		Color = c.getColor();
-		if (Color[0] != 0.2755 || Color[1] != 0.2794 || Color[2] !=  0.2451) {
-			
+		if (!(errorrange(distance,c.getDistance(),error))) {
+			return false;
 		}
-		System.out.println("Bleu");
-		Button.ENTER.waitForPressAndRelease();
-		Color = c.getColor();
-		if (Color[0] != 0.0235 || Color[1] != 0.0441 || Color[2] !=  0.0696) {
-			
-		}
+		return true;
 		
+	}
+	
+	public static boolean TestColor(Capteur c, float error) {
+		float[] Color = new float[3];
+		System.out.println("Test Blanc");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(White[0],Color[0],error) || !errorrange(White[1],Color[1],error) || !errorrange(White[2],Color[2],error)) {
+			return false;
+		}
+		System.out.println("Test Bleu");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(White[0],Color[0],error) || !errorrange(White[1],Color[1],error) || !errorrange(White[2],Color[2],error)) {
+			return false;
+		}
+		System.out.println("Test Jaune");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(Yellow[0],Color[0],error) || !errorrange(Yellow[1],Color[1],error) || !errorrange(Yellow[2],Color[2],error)) {
+			return false;
+		}
+		System.out.println("Test Rouge");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(Red[0],Color[0],error) || !errorrange(Red[1],Color[1],error) || !errorrange(Red[2],Color[2],error)) {
+			return false;
+		}
+		System.out.println("Test Vert");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(Green[0],Color[0],error) || !errorrange(Green[1],Color[1],error) || !errorrange(Green[2],Color[2],error)) {
+			return false;
+		}
+		System.out.println("Test Noir");
+		Button.ENTER.waitForPressAndRelease();
+		Color = c.getColor();
+		if (!errorrange(White[0],Color[0],error) || !errorrange(White[1],Color[1],error) || !errorrange(White[2],Color[2],error)) {
+			return false;
+		}
 		return true;
 	}
 	
@@ -82,12 +128,6 @@ public class TestCalibration {
 	}
 
 	public static void main(String[] args) throws SocketException, UnknownHostException {
-		//Capteur c = new Capteur();
-		Actionneur a = new Actionneur();
-		
-		TestRotation(a,200);
-		
-
 	}
 
 }
